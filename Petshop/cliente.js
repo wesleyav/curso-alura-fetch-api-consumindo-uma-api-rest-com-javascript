f476b8bd88cd122d0ac967e6b845adb3c898d676
Wesley <wesley.contato@hotmail.com>
2020-08-10 05:56:52 -0300

    Apresentando Template literals

--- Petshop/cliente.js
+++ Petshop/cliente.js
@@ -4,3 +4,15 @@ const informacoesClientes = [
         nome: "Jairo"
     }
 ]
+
+const conteudoLInha = `
+<tr>
+        <td>${informacoesClientes[0].cpf}</td>
+        <td>${informacoesClientes[0].nome}</td>
+      </tr>
+      `
+
+
+const corpoTabela = document.querySelector("[data-conteudo-tabela]")
+
+corpoTabela.innerHTML = conteudoLInha
